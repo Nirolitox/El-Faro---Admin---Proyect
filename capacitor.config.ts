1345abb478d585cda82cebc191115cee6b680dba
Rafa | DEV <rafaelalejandrocastellano05@gmail.com>
2024-11-13 11:06:47 -0400

    First commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Sio',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
